2fd90acc1d12e41b1dd91ea293710f205cc8e439
Fg120 <anwaria1404@gmail.com>
2023-04-04 11:50:06 +0700

    On branch master 	new file:   public/img/home/logo.png 	modified:   resources/views/includes/footer.blade.php 	modified:   resources/views/includes/header.blade.php 	modified:   resources/views/includes/link.blade.php 	modified:   routes/web.php

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,74 @@
+<div class="container" id="footer">
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        <div class="col mb-3">
+            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+                <img src="{{ asset('img/home/logo.png') }}" alt="Adventra" width="40" height="40">
+            </a>
+            <p class="text-body-secondary">©Adventra 2023</p>
+        </div>
+
+        <div class="col mb-3">
+
+        </div>
+
+        <div class="col mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
+
+        <div class="col mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
+
+        <div class="col mb-3">
+            <h5>Contacts</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        <i class="bi bi-whatsapp" width="24" height="24"></i> +62 812-3456-7890
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        <i class="bi bi-instagram" width="24" height="24"></i> @Adventra
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        <i class="bi bi-envelope" width="24" height="24"></i> adventra@gmail.com
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        {{-- <div class="col mb-3">
+            <h5>Contact</h5>
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li class="ms-3">
+                    <a class="text-body-secondary" href="#">
+                        <i class="bi bi-whatsapp" width="24" height="24"></i>
+                        <p>+62 812-3456-7890</p>
+                    </a>
+                </li>
+                <li class="ms-3">
+                    <a class="text-body-secondary" href="#">
+                        <i class="bi bi-instagram" width="24" height="24"></i>
+                    </a>
+                </li>
+            </ul>
+        </div> --}}
+    </footer>
+</div>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,21 +1,23 @@
-<nav class="navbar bg-body-tertiary">
-    <div class="container">
-        <a class="navbar-brand" href="{{ route('home.index') }}">
-            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
-        </a>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+<nav class="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
+            <div class="col-md-6">
+                <a class="navbar-brand" href="{{ route('home.index') }}">
+                    <img src="{{ asset('img/home/logo.png') }}" alt="Bootstrap" width="30" height="30">
+                </a>
+            </div>
+            <ul class="navbar-nav col-lg-6 justify-content-lg-end">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home.index') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Tentang Kami</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#">Kontak</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link" href="#">FAQ</a>
                 </li>
             </ul>
         </div>
