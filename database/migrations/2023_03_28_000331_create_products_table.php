8020692952d229ed82e2c0bd46b5cc82a15009a2
Raditya <raditya.mn99@gmail.com>
2023-04-12 12:18:21 +0700

    master

--- database/migrations/2023_03_28_000331_create_products_table.php
+++ database/migrations/2023_03_28_000331_create_products_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-<<<<<<<< HEAD:database/migrations/2023_04_08_093241_add_column_to_users_table.php
+
         Schema::table('users', function (Blueprint $table) {
             $table->drop(['name']);
-========
+
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -26,7 +26,7 @@ return new class extends Migration
             $table->string('stock_available');
             $table->string('stock_true');
             $table->timestamps();
->>>>>>>> 5c9d7987ff36c87bbcd4f8c4c3011b1b3011c7b4:database/migrations/2023_03_28_000331_create_products_table.php
+
         });
     }
 
@@ -37,14 +37,11 @@ return new class extends Migration
      */
     public function down()
     {
-<<<<<<<< HEAD:database/migrations/2023_04_08_093241_add_column_to_users_table.php
+
         Schema::table('users', function (Blueprint $table) {
             Schema::table('users', function (Blueprint $table) {
                 $table->drop(['name']);
             });
         });
-========
-        Schema::dropIfExists('products');
->>>>>>>> 5c9d7987ff36c87bbcd4f8c4c3011b1b3011c7b4:database/migrations/2023_03_28_000331_create_products_table.php
     }
 };
