0c1191788cfdc02b643668c0175a80f0504efe3f
Raditya <raditya.mn99@gmail.com>
2023-04-12 12:28:27 +0700

    master

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,13 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
->>>>>>> 5c9d7987ff36c87bbcd4f8c4c3011b1b3011c7b4
 use App\Models\User;
-
 class RegisterController extends Controller
 {
     public function create()
@@ -23,10 +17,6 @@ class RegisterController extends Controller
         'email'=>'required|email|unique:users,email',
         'password'=>'required|min:8',
         ]);
-<<<<<<< HEAD
-        
-=======
->>>>>>> 5c9d7987ff36c87bbcd4f8c4c3011b1b3011c7b4
 
         $attributes['password'] = bcrypt($attributes['password']);
 
@@ -39,54 +29,3 @@ class RegisterController extends Controller
         return redirect('/')->with('success', 'Yout account has been created!');;
     }
 }
-<<<<<<< HEAD
-=======
-use App\Models\Register;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\facades\storage;
-
-class RegisterController extends Controller
-{
-    public function index(Request $request)
-    {
-        //dd($request->all());
-        $users = User::all();
-        return view('register.create');
-     
-
-        
-    }
-    public function store(Request $request)
-    {
-        // dd($request);
-        $file =$request->file('photo');
-        $filename = time() . '.' . $file->getClientOriginalExtension();
-        $path =$request->file('photo')->storeAs('public/ktp',$filename);
-
-        $path = str_replace('public/','',$path);
-        
-        //dd($path);
-        $data = [
-            'firstname' => $request->firstname,
-            'lastname' => $request->NamaBelakang,
-            'email' => $request->Email,
-            'phonenumber' => $request->NoTelp,
-            'password' => Hash::make($request->password),
-            'province' => $request->Provinsi,
-            'city' => $request->Kabupaten,
-            'district' => $request->Kecamatan,
-            'village' => $request->Kelurahan,
-            'address' => $request->Alamat,
-            'gender' => $request->JenisKelamin,
-            'nik' => $request->NoNIK,
-            'photo' => $path
-        ];
-        $users = User::create($data);
-        
-    }
-    }
->>>>>>> Stashed changes
-=======
->>>>>>> 5c9d7987ff36c87bbcd4f8c4c3011b1b3011c7b4
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,11 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-<<<<<<< HEAD
-use App\Http\Controllers\RegisterController;
-=======
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UsersController;
->>>>>>> 5c9d7987ff36c87bbcd4f8c4c3011b1b3011c7b4
+use App\Http\Controllers\RegisterController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,11 +19,11 @@ use App\Http\Controllers\UsersController;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
-<<<<<<< HEAD
+
 Route::get('/register', [RegisterController::class, 'index']) -> name('register');
 Route::POST('/register/store', [RegisterController::class, 'store'])->name('register.store');
 
-=======
+
 
 Route::get('login', [LoginController::class, 'create'])->name('login');
 Route::post('login', [LoginController::class, 'store'])->name('login');
@@ -40,4 +36,4 @@ Route::post('register', [RegisterController::class, 'store'])->name('register');
 //ROUTE ADMIN
 Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard')->middleware('auth');
 Route::get('admin/users', [UsersController::class, 'index'])->name('admin.users')->middleware('auth');
->>>>>>> 5c9d7987ff36c87bbcd4f8c4c3011b1b3011c7b4
+
