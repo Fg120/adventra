03f4d03aa7fc3d3561446fa538a9c5ab784a1d86
Raditya <raditya.mn99@gmail.com>
2023-05-09 20:55:48 +0700

    master

--- resources/views/home/show_order.blade.php
+++ resources/views/home/show_order.blade.php
@@ -44,14 +44,69 @@
                                             <button type="submit" class="btn btn-primary mt-3">Submit payment</button>
                                         </form>
                                     @endif
-                                    <br>
-                                    <a href="{{route('home.index')}}" class="btn btn-primary">Home</a>
+                                    
+                                 
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section><!-- End Portfolio Section -->
+                <section id="services" class="services section-bg">
+                    <div class="container" data-aos="fade-up">
+        
+        
+        
+                        <div class="section-title">
+                            <h2>PEMBAYARAN ADVENTRA</h2>
+                            
+                            <h4> Anda Dapat Melakukan Pembayaran Dengan Mentransfer Ke Rekening Bank Kami Dibawah Ini</h4>
+                            
+                            
+                        </div>
+        
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                                <div class="icon-box">
+                                    <div class="icon"><i class="bx bxl-dribbble"></i></div>
+                                    <h4><a href="">MANDIRI</a></h4>
+                                    <p>Anda Bisa Memesan Produk yang dijual dan disewa secara online. Tanpa Ribet, mudah, cepet,
+                                        efisien, dimana saja & kapan saja</p>
+                                </div>
+                            </div>
+        
+                            <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in"
+                                data-aos-delay="200">
+                                <div class="icon-box">
+                                    <div class="icon"><i class="bx bx-file"></i></div>
+                                    <h4><a href="">BCA</a></h4>
+                                    <p>Tanpa harus menghubungi admin, Anda bisa cek ketersedian produk sewa, lihat di detail
+                                        produk dan sesuaikan jadwal Anda sebelum kehabisan</p>
+                                </div>
+                            </div>
+        
+                            <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
+                                data-aos-delay="300">
+                                <div class="icon-box">
+                                    <div class="icon"><i class="bx bx-tachometer"></i></div>
+                                    <h4><a href="">CIMB</a></h4>
+                                    <p>Peralatan yang berkualitas, bersih dan nyaman untuk dipakai. Rasakan pengalaman seperti
+                                        menginap di hotel</p>
+                                </div>
+                            </div>
+        
+                            <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in"
+                                data-aos-delay="400">
+                                <div class="icon-box">
+                                    <div class="icon"><i img src="img/home/header.png" class="img-fluid animated" alt="></i></div>
+                                    <h4><a href="">BRI</a></h4>
+                                    <p>Fitur harga dinamis untuk produk sewa dimana Anda akan mendapatkan potongan harga
+                                        langsung untuk pemesanan lebih dari satu hari</p>
+                                </div>
+                            </div>
+                            
+                        </div>
+        
+                    </div>
         </section>
     </main><!-- End #main -->
 @endsection
